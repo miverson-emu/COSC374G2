e0f789b2c04fed90e7190b01838a4a892a873651
yu-zha <yzhang32@emich.edu>
2021-10-16 19:27:54 -0400

    Yu - 10/16 Normal App Update the nav feature

--- normal/app/src/main/java/com/example/login_2/MainActivity.java
+++ normal/app/src/main/java/com/example/login_2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.login_2;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TestActivity.class);
+            startActivity(intent);
+        });
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
